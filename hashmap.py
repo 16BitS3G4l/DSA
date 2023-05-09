596846755771b8a8b7994ec9e9058d2401c8163a
dev-segal <ysegal7@student.tuoro.edu>
2023-05-09 18:16:20 -0400

    Implementation of a Hash Map in Python

---
+++ hashmap.py
@@ -0,0 +1,18 @@
+# Implementation of a HashMap (String data types only) Data Structure 
+class HashMap:
+
+    def __init__(self, size=10):
+        self.values = [0 for x in range(0, size)]
+        self.size = size 
+        print(self.values)
+
+    def hash(self, key):
+        return len(key)
+    
+    def put(self, key, value):
+        index = self.hash(key)
+        self.values.insert(index, value)
+
+    def get(self, key):
+        index = self.hash(key)
+        return self.values[index]
