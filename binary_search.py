2566b446691f8e4781f444596aba48453b5f5b59
dev-segal <ysegal7@student.tuoro.edu>
2023-05-10 16:32:44 -0400

    Implementation of Bianry Search in Python

--- binary_search.py
+++ binary_search.py
@@ -1,5 +1,23 @@
-def solve(listToSearch=[], elementToSearch=0):
-    pass
+# Implementation of Vanilla Binary Search 
+import math 
 
+def solveHelper(listToSearch=[], elementToSearch=0):
+    return solve(listToSearch, elementToSearch, 0, len(listToSearch)-1) 
 
-solve([1, 2, 3, 4], 2)
+def solve(listToSearch=[], elementToSearch=0, start=0, end=0):
+    
+    midway_point = math.floor((start+end)/2)
+
+    if(listToSearch[midway_point] == elementToSearch):
+        return midway_point 
+    
+    elif(end == start):
+        return -1 
+    
+    elif listToSearch[midway_point] < elementToSearch:
+        return solve(listToSearch, elementToSearch, midway_point+1, end)
+    
+    elif listToSearch[midway_point] > elementToSearch:
+        return solve(listToSearch, elementToSearch, start, midway_point-1)
+    
+
