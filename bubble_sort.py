2e092c59f8b3fef5b88938c91e687d10a0e3f57d
dev-segal <ysegal7@student.tuoro.edu>
2023-05-10 16:49:16 -0400

    Implementation of Bubble Sort in Python

---
+++ bubble_sort.py
@@ -0,0 +1,19 @@
+def sort(listToBeSorted=[]):
+
+    # set to True so it passes at least once
+    swapped = True
+    
+    while swapped:
+        
+        # reset swapped so it doesn't go on forever 
+        swapped = False 
+
+        for x in range(0, len(listToBeSorted)-1):
+            
+            if(listToBeSorted[x] > listToBeSorted[x+1]):
+                
+                tempX = listToBeSorted[x]
+                listToBeSorted[x] = listToBeSorted[x+1]
+                listToBeSorted[x+1] = tempX
+                swapped = True
+
