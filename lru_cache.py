1a674449a7e90b32bab4693d3419e925b8d5b170
dev-segal <ysegal7@student.tuoro.edu>
2023-05-31 14:34:00 -0400

    Finished LRU Cache Implementation with OrderedDict

--- lru_cache.py
+++ lru_cache.py
@@ -1,11 +1,12 @@
 from linked_list import Node
+from collections import OrderedDict
 
 class LRUCache:
 
     def __init__(self, cache_size=4):
-        self.elements = {}
+        self.elements = OrderedDict()
         self.linked_elements = Node("FIRST!")
-
+        self.cache_size = cache_size
 
     def changeCacheSize(self, cache_size):
         self.cache_size = cache_size 
@@ -18,20 +19,21 @@ class LRUCache:
         if len(self.elements) == self.cache_size:
             last_cache_entry = next(reversed(self.elements.keys()))
             
-            # node before the last node in the linked list 
             prev_node = self.elements[last_cache_entry].getPrev()
 
             if prev_node is not None:
-                prev_node.setNext(None)
+                prev_node.changeNext(None)
 
             del self.elements[last_cache_entry] 
 
-        temp_node = Node(dataRequested, self.linked_elements)
 
+        temp_node = Node(dataRequested, self.linked_elements)
 
         self.linked_elements.setPrev(temp_node)
 
         self.elements[dataRequested] = temp_node
+        self.elements.move_to_end(dataRequested, last=False)
+
         self.linked_elements = temp_node
 
         return self.linked_elements
@@ -39,9 +41,15 @@ class LRUCache:
 
     def cacheHit(self, dataRequested, hit):        
         
+        del self.elements[dataRequested]
+
         # cache hit
         tempNode = Node(dataRequested, self.linked_elements)
-        
+                
+        self.elements[dataRequested] = tempNode
+
+        self.elements.move_to_end(dataRequested, last=False)
+
         self.linked_elements.setPrev(tempNode)
         self.linked_elements = tempNode
 
@@ -58,8 +66,3 @@ class LRUCache:
 
         # cache hit       
         return self.cacheHit(dataRequested, cache_query) 
-
-            
-            
-
-
